823b7b357e35ccd88a4865f76b95729d05d502dc
Voxuanan <voxuanan0307@gmail.com>
2022-07-20 15:42:41 +0700

    first commit

---
+++ 3.Publish_Subscribe/emit_log.js
@@ -0,0 +1,27 @@
+var amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://localhost", function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var exchange = "logs";
+        // Using process agrument 2 to change our logs if needed
+        var msg = process.argv.slice(2).join(" ") || "Hello World!";
+
+        // Use "fanout" to tell exchange send to all queues is connected to
+        channel.assertExchange(exchange, "fanout", {
+            durable: false,
+        });
+        channel.publish(exchange, "", Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+    });
+
+    setTimeout(function () {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
